e64750f62c209fbbdf9aced956944baf8685c626
emejulucodes <emmanueljunior433@gmail.com>
2022-08-31 01:12:53 +0100

    Changed Numbers

--- classes/wordpress/script.js
+++ classes/wordpress/script.js
@@ -19,10 +19,10 @@ window.addEventListener("load", () => {
     email = document.querySelector("#email").value;
     clas = "Mobile App Development";
     phonenumber = document.querySelector("#phone").value;
-    if (skills === "") {  
-        message = `https://wa.me/2348068597140?text=Hello%2C%20I%20just%20registered%20for%20${clas}.%0AHere%20are%20my%20details%3A%0AName%3A%20${firstname} ${lastname}.%0AEmail%3A%20${email}.%0APhone%20Number%3A%20${phonenumber}.%0AAddress%3A%20${address}.`
+    if (skills) {  
+        message = `https://wa.me/2348188578955?text=Hello%2C%20I%20just%20registered%20for%20${clas}.%0AHere%20are%20my%20details%3A%0AName%3A%20${firstname} ${lastname}.%0AEmail%3A%20${email}.%0APhone%20Number%3A%20${phonenumber}.%0AAddress%3A%20${address}.`
     } else {
-        message = `https://wa.me/2348068597140?text=Hello%2C%20I%20just%20registered%20for%20${clas}.%0AHere%20are%20my%20details%3A%0AName%3A%20${firstname} ${lastname}.%0AEmail%3A%20${email}.%0APhone%20Number%3A%20${phonenumber}.%0AAddress%3A%20${address}.%0ASkills%3A%20${skills}.`
+        message = `https://wa.me/2348188578955?text=Hello%2C%20I%20just%20registered%20for%20${clas}.%0AHere%20are%20my%20details%3A%0AName%3A%20${firstname} ${lastname}.%0AEmail%3A%20${email}.%0APhone%20Number%3A%20${phonenumber}.%0AAddress%3A%20${address}.%0ASkills%3A%20${skills}.`
     }
     window.open(message);
   });
